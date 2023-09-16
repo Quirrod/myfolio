af7e331d660e3450b586d54142e3c1dca8d7e84c
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-09-16 18:12:23 +0300

    ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,14 +4,8 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    screens: {
-      sm: "540px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
-    fontSize: {
+    extend: {
+      fontSize: {
       xs: [
         "0.75rem",
         {
@@ -91,27 +85,11 @@ module.exports = {
         },
       ],
     },
-    extend: {
 
-      colors: {
-
-        blue: {
-         50: "#EBEBFF",
-      100: "#D2D2FE",
-      200: "#A6A4FE",
-      300: "#7E7CFD",
-      400: "#524FFD",
-      500: "#2522FC",
-      600: "#0703E2",
-      700: "#0502AB",
-      800: "#03026F",
-      900: "#020137",
-      950: "#01001E"
-        },
-      },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+        serif: ["Newsreader", ...defaultTheme.fontFamily.serif],
+
       },
     },
   },
