75df5eb57f9bca12787b5c059cb27283b4614b8d
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-10-27 10:58:17 -0400

    fix images

--- src/data/work.json.ts
+++ src/data/work.json.ts
@@ -6,20 +6,20 @@ export interface Template {
 };
 const one: Template = {
   link: "https://bugaboo-kohl.vercel.app",
-  img: "/bugaboo-page.png",
+  img: "https://quirrod.github.io/myfolio/bugaboo-page.png",
   title: "Bugabooo",
   description: "Bugaboo is a dynamic and innovative web application designed to revolutionize the way developers collaborate, share code, and receive valuable feedback."
 };
 const two: Template  = {
   link: "https://ensolver-challenge.vercel.app",
-  img: "/ensolver.png",
+  img: "https://quirrod.github.io/myfolio/ensolver.png",
   title: "Knock Note",
   description: 'KnockNote is a simple note taking app'
 };
 
 const three: Template  = {
   link: "A simple and beatiful Movie directory",
-  img: "/movielog.png",
+  img: "https://quirrod.github.io/myfolio/movielog.png",
   title: "Movie Log",
   description: 'A simple and beatiful Movie directory'
 };
