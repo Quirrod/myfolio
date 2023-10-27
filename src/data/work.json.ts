ac669276ba911916d2bb47bef1555708b7f91ec5
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-10-27 10:41:35 -0400

    me

--- src/data/work.json.ts
+++ src/data/work.json.ts
@@ -1,113 +1,32 @@
 export interface Template {
   link: string;
   title: string;
-
   img: string;
+  description: string;
 };
 const one: Template = {
-  link: "#",
-  img: "/1.png",
-  title: "Lexington Themes",
+  link: "https://bugaboo-kohl.vercel.app",
+  img: "/bugaboo-page.png",
+  title: "Bugabooo",
+  description: "Bugaboo is a dynamic and innovative web application designed to revolutionize the way developers collaborate, share code, and receive valuable feedback."
 };
-const two = {
-  link: "#",
-  img: "/2.png",
-  title: "Creative Solutions",
-};
-
-const three = {
-  link: "#",
-  img: "/3.png",
-  title: "Digital Innovations",
+const two: Template  = {
+  link: "https://ensolver-challenge.vercel.app",
+  img: "/ensolver.png",
+  title: "Knock Note",
+  description: 'KnockNote is a simple note taking app'
 };
 
-const four = {
-  link: "#",
-  img: "/4.png",
-  title: "WebCrafters",
+const three: Template  = {
+  link: "A simple and beatiful Movie directory",
+  img: "/movielog.png",
+  title: "Movie Log",
+  description: 'A simple and beatiful Movie directory'
 };
-
-const five = {
-  link: "#",
-  img: "/5.png",
-  title: "PixelPerfect Designs",
-};
-
-const six = {
-  link: "#",
-  img: "/6.png",
-  title: "CodeMasters",
-};
-
-const seven = {
-  link: "#",
-  img: "/7.png",
-  title: "Web Wizards",
-};
-
-const eight = {
-  link: "#",
-  img: "/8.png",
-  title: "TechCreators",
-};
-
-const nine = {
-  link: "#",
-  img: "/9.png",
-  title: "WebArtistry",
-};
-
-const ten = {
-  link: "#",
-  img: "/10.png",
-  title: "DigitalCraftsmen",
-};
-const eleven = {
-  link: "#",
-  img: "/11.png",
-  title: "SiteSculptors",
-};
-const twelve = {
-  link: "#",
-  img: "/12.png",
-  title: "WebWorks Pro",
-};
-
-const thirteen = {
-  link: "#",
-  img: "/13.png",
-  title: "CodeArtisans",
-};
-
-const fourteen = {
-  link: "#",
-  img: "/14.png",
-  title: "TechFusion",
-};
-
-const fifteen = {
-  link: "#",
-  img: "/15.png",
-  title: "DigitalCrafters",
-};
-
 
 export const bytitle = {
   one,
-    two,
-    three,
-  four,
-five,
-six,
-seven,
-eight,
-  nine,
-  ten,
-  eleven,
-  thirteen,
-  twelve,
-  fourteen,
-  fifteen,
-
+  two,
+  three,
 };
 export const work = Object.values(bytitle);
